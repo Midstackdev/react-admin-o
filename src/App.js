797c4b0493ea1d033d258b2d96b1d6bf8672a7fc
Midstackdev <midstackdev@gmail.com>
2021-07-01 22:52:58 +0000

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <div className="">
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">
+          Others go here
+        </div>
+      </div>
     </div>
   );
 }
